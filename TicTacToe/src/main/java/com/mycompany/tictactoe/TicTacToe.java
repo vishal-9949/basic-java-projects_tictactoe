bbd041c03ee436d821205cba7fdd552dc7a3ff1c
vishal-9949 <vishaltogalwar2@gmail.com>
2025-09-25 17:59:41 +0530

    upload file

---
+++ TicTacToe/src/main/java/com/mycompany/tictactoe/TicTacToe.java
@@ -0,0 +1,81 @@
+
+package com.mycompany.tictactoe;
+import java.util.*;
+
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        for(int row=0; row< board.length; row++){
+            for(int col=0; col < board[row].length; col++){
+                board[row][col] = ' ';
+            }
+        }
+        
+        char player ='X'; //defult player x
+        boolean gameOver = false; //first condition for game
+        
+        Scanner sc = new Scanner(System.in);
+        
+        
+        while(!gameOver){
+            printBoard(board);
+            System.out.print("Player "+player+" Enter: ");
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+            
+            if(board[row][col]==' '){
+                board[row][col]=player;//place the element
+                gameOver = haveWon(board, player);
+                if(gameOver){
+                    System.out.println("Player"+player+" has Won the game!!!!!!!!!!");
+                }else{
+                    if(player == 'X'){
+                        player = 'O';
+                    }else{
+                        player = 'X';
+                    }
+                }
+            }else{
+                System.out.println("Invalid move");
+            }
+        }
+        printBoard(board);
+         
+    }
+    
+    
+    public static void printBoard(char[][] board){
+     for(int row=0; row< board.length; row++){
+            for(int col=0; col < board[row].length; col++){
+                System.out.print(board[row][col]+" | ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static boolean haveWon(char[][] board, char player){
+    //check for row
+    for(int row=0; row< board.length; row++){
+        if(board[row][0]==player && board[row][1]==player && board[row][2]==player){
+            return true;
+        }
+    }
+    //chack for colum
+    for(int col=0; col < board[0].length; col++){
+        if(board[0][col]==player && board[1][col]==player && board[2][col]==player){
+            return true;
+        }
+    }
+    //check for diagonal
+    
+    if(board[0][0] == player && board[1][1]== player && board[2][2]==player){
+        return true;
+    }
+    if(board[0][2] == player && board[1][1]== player && board[2][0]==player){
+        return true;
+    }
+    return false;
+}
+}
